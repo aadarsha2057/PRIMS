443b2c3db32ad9cafb7767fe2a248890b2885584
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-12 22:24:26 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-import networkx as nx
+import networky as ny
 from functions import *
-from algorithms import prims_algorithm
+from algorithms import prims_alg
 from drawing import *
 
 
-graph_data = open('test-graphs/G1.txt', 'r')
+graph_data = open('Graphs/graph1.txt', 'r')
 
-G=nx.read_weighted_edgelist(graph_data, nodetype=int)
+G=ny.read_weighted_edgelist(graph_data, nodetype=int)
 
-T= prims_algorithm(G, 0, show_graph = True, show_cost = True)
+A= prims_alg(G, 0, show_graph = True, show_cost = True)
