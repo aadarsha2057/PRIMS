66cc05716660bde692e166a9c7c3695a35f7eb2f
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-13 11:29:12 -0600

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from algorithms import prims_alg
 from drawing import *
 
 
-graph_data = open('Graphs/graph1.txt', 'r')
+graph_data = open('Graphs/graph01.txt', 'r')
 
 G=ny.read_weighted_edgelist(graph_data, nodetype=int)
 
