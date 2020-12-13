031271aeef2b165950c226fc78b8f8989bb2d26e
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-13 11:28:49 -0600

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -10,15 +10,15 @@ def prims_algorithm(G, start_vert, show_g, show_c):
         print(A)
         return A
         
-    while is_span(G,A)==False: # to check functions
+    while is_span(G,A)==False: #to check functions
         e = min_prims_edge(G,A)
         A.add_edge(e[0],e[1])
         
-    if show_g == True: # to display a visual representation of the subtree
+    if show_g == True: #to display a visual representation of the subtree
         show_weighted_graph(G)
         draw_subtree(G,A)
         
-    if show_c == True: # to display the weight of the graph
+    if show_c == True: #to display the weight of the graph
         total_c = 0
         for y in E(A): #to add up all the weights
             total_c = total_c + c(G,x)
