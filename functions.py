b82083a3d0f62b95e0526e981bdd474d47949845
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-12 22:41:50 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,35 +1,35 @@
 import networkx as ny
 
 def V(graph):
-    return set(graph.nodes()) #list of vertices
+    return set(graph.nodes()) #list of the vertices
 
 def E(graph):
-    return set(graph.edges()) #list of edges
+    return set(graph.edges()) #list of the edges
 
-def prims_ini(graph, v ): #checks if the starting verticy exists
+def prims_ini(graph, v ): #to check if the starting vertices exist
     if(v not in V(graph)):
-        return 'Error: Vertex NOT Found' #error message
-    else: #initializes the subtree
+        return 'Error: Vertex NOT Found' #if there's an error
+    else: #to initialize the subtree
         A=nx.Graph()
         A.add_node(v)
         return A
 
-def is_span(graph, subgraph): # makes sure the subtree doesn't include cycles or all edges of G
+def is_span(graph, subgraph): #to make sure that the subtree does not include cycles or all the edges of G
     return V(graph)==V(subgraph)
 
-def cost(G, e): #return the weight of the edge
+def cost(G, e): #to return the weight of the edge
     return G[e[0]][e[1]]['weight']
 
-def possible_edges(G,A): #checks to see what edges don't create a cycle
+def possible_edges(G,A): #to check to see what edges do not create a cycle
     return [e for e in list(G.edges(V(A))) if e[0] not in V(A) or e[1] not in V(A)]
 
 def min_prims_edge(G,A):
     possible = possible_edges(G, A)
-    minimum = 9999999999 #used to check for minimum default set to a really high num
-    next_edge = list() #used to hold the edge that has the minimum weight
-    for y in range(0,len(possible),1): #runs through the list of edges
-        weight = cost(G,possible[y]) #keeps track of the weight
-        if weight < minimum: #compare the weight of the edge to current min
-            minimum = cost(G,possible[y]) #set the min to the smallest weight
-            next_edge = possible[y] #save the edge with small weight
-    return next_edge #return the smallest edge
+    minimum = 9999999999 #to check for minimum default set to a really high number
+    next_edge = list() #to hold the edge that has the minimum weight
+    for y in range(0,len(possible),1): #to run through the list of edges
+        weight = cost(G,possible[y]) #to keeps track of the weight
+        if weight < minimum: #to compare the weight of the edge to current min
+            minimum = cost(G,possible[y]) #to set the min to the smallest weight
+            next_edge = possible[y] #to save the edge with small weight
+    return next_edge #to return the smallest edge
