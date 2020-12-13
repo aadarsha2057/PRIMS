fa803148a9ebfd11b78dfd04d56f4ca4fe6cb86c
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-12 22:23:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import networkx as nx
+import networkx as ny
 from functions import * 
 from drawing import *
 from algorithms import *
 
-graph_data = open('test-graphs/G4.txt','r') #read in the graph you want to test G1-G4
+graph_data = open('Graphs/graph4.txt','r') #read in the graph you want to test graph1 - graph4
 
-G = nx.read_weighted_edgelist(graph_data, nodetype = int) #create initial graph 
+G = ny.read_weighted_edgelist(graph_data, nodetype = int) #create initial graph 
 
-T = prims_algorithm(G, int(input("Type starting vertex: ")), True, True) #ask the user what verticy they want to start on
+A = prims_alg(G, int(input("Type starting vertex: ")), True, True) #ask the user what verticy they want to start on
