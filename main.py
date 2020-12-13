7a658abb0ea0cc27b400eb7a23d2832e29126444
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-12 22:42:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from functions import *
 from drawing import *
 from algorithms import *
 
-graph_data = open('Graphs/graph4.txt','r') #read in the graph you want to test graph1 - graph4
+graph_data = open('Graphs/graph4.txt','r') #to read in the graph you want to test graph1 - graph4
 
-G = ny.read_weighted_edgelist(graph_data, nodetype = int) #create initial graph 
+G = ny.read_weighted_edgelist(graph_data, nodetype = int) #to create the initial graph 
 
-A = prims_alg(G, int(input("Type starting vertex: ")), True, True) #ask the user what verticy they want to start on
+A = prims_alg(G, int(input("Type starting vertex: ")), True, True) #to ask the user what verticy they want to start on
