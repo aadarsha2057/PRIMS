9a7691b7b3c0f6460f77c074e12434a120d03f80
aadarsha2057 <75274804+aadarsha2057@users.noreply.github.com>
2020-12-12 22:38:50 -0600

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -6,21 +6,21 @@ def prims_algorithm(G, start_vert, show_g, show_c):
     
     A = prims_ini(G, start_vert)
     
-    if isinstance(A,str) == True: #checks if the vertex exists in the graph
+    if isinstance(A,str) == True: #to check if the vertex exists in the graph
         print(A)
         return A
         
-    while is_span(G,A)==False: #check functions
+    while is_span(G,A)==False: # to check functions
         e = min_prims_edge(G,A)
         A.add_edge(e[0],e[1])
         
-    if show_g == True: #display a visual representation of the subtree
+    if show_g == True: # to display a visual representation of the subtree
         show_weighted_graph(G)
         draw_subtree(G,A)
         
-    if show_c == True: #display the weight of the graph
+    if show_c == True: # to display the weight of the graph
         total_c = 0
-        for y in E(A): #add up all the weights
+        for y in E(A): #to add up all the weights
             total_c = total_c + c(G,x)
         print("the cost of the spanning tree is: " + str(total_c))
             
